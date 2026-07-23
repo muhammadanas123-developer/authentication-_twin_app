c24c1c8b6512923d983b952611a09e78cb9f4737
Muhammad Anas <muhanmadanas8@gmail.com>
2026-07-23 15:26:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.post("/verify-otp", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
